5b009131237650f916eaaaaadb31e43b088fd8e1
Mariana Marca <mariana.marca.herrera.2003@gmail.com>
2024-07-03 18:07:05 -0400

    boton categoria

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -26,6 +26,11 @@
                         {{ __('Registrar') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link href="{{ route('producto.crear') }}" :active="request()->routeIs('producto.crear')">
+                        {{ __('categoria') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ms-6">
