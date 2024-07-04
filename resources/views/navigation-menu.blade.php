caaab319b6bcd8b8a0bc01a02040ffbb7876cf1e
Mariana Marca <mariana.marca.herrera.2003@gmail.com>
2024-07-04 13:14:27 -0400

    modificar2

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -22,13 +22,18 @@
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link href="{{ route('producto.crear') }}" :active="request()->routeIs('producto.crear')">
-                        {{ __('Registrar') }}
+                    <x-nav-link href="{{ route('roles.principal') }}" :active="request()->routeIs('roles.principal')">
+                        {{ __('Rol') }}
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link href="{{ route('producto.crear') }}" :active="request()->routeIs('producto.crear')">
-                        {{ __('categoria') }}
+                    <x-nav-link href="{{ route('categorias.principal') }}" :active="request()->routeIs('categorias.principal')">
+                        {{ __('Categoria') }}
+                    </x-nav-link>
+                </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link href="{{ route('perfiles.principal') }}" :active="request()->routeIs('perfiles.principal')">
+                        {{ __('Perfil') }}
                     </x-nav-link>
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\categoriaController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\inicioController;
+use App\Http\Controllers\perfilesController;
 use App\Http\Controllers\productoController;
+use App\Http\Controllers\rolController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,31 +19,6 @@ use App\Http\Controllers\productoController;
 */
 
 Route::get('/', inicioController::class);
-
-
-
-Route::controller(productoController::class)->group(function(){
-
-    route::get('producto', 'principal')->name('producto.principal');
-
-    route::get('producto/crear', 'crear')->name('producto.crear');
-
-    route::post('producto','store')->name('producto.store');
-
-    Route::get('producto/{variable}/mostrar', 'mostrar')->name('producto.mostrar');
-
-    route::get('producto/{producto}/edit', 'editar')->name('producto.editar');
-
-    route::put('producto/{producto}', 'update')->name('producto.update');
-    
-    route::delete('producto/{id}', 'borrar')->name('producto.borrar');
-
-    route::get('desactivar/{id}', 'desactivarproducto')->name('desactivapro');
-
-    route::get('activa/{id}', 'activaproducto')->name('activapro');
-});
-
-
 
 Route::middleware([ 'auth:sanctum', config('jetstream.auth_session'), 'verified',])->group(function () {
     
@@ -66,5 +44,70 @@ Route::middleware([ 'auth:sanctum', config('jetstream.auth_session'), 'verified'
     
         route::get('activa/{id}', 'activaproducto')->name('activapro');
     });
+    Route::controller(rolController::class)->group(function(){
+
+        route::get('rol', 'principal')->name('roles.principal');
+
+        route::get('rol/crear', 'crear')->name('roles.crear');
+
+        route::post('rol','store')->name('roles.store');
     
+        route::get('rol/{variable}/mostrar', 'mostrar')->name('roles.mostrar');
+
+        route::get('rol/{rol}/edit', 'editar')->name('roles.editar');
+
+        route::delete('rol/{id}', 'borrar')->name('roles.borrar');
+
+        route::put('rol/{rol}', 'update')->name('roles.update');
+        
+        route::delete('rol/{id}', 'borrar')->name('roles.borrar');
+    
+        route::get('desactivarrol/{id}', 'desactivarrol')->name('desactivarol');
+    
+        route::get('activarol/{id}', 'activarol')->name('activarol');
+    });
+    Route::controller(categoriaController::class)->group(function(){
+
+        route::get('categoria', 'principal')->name('categorias.principal');
+
+        route::get('categoria/crear', 'crear')->name('categorias.crear');
+
+        route::post('categoria','store')->name('categorias.store');
+    
+        route::get('categoria/{variable}/mostrar', 'mostrar')->name('categorias.mostrar');
+
+        route::get('categoria/{categoria}/edit', 'editar')->name('categorias.editar');
+
+        route::delete('categoria/{id}', 'borrar')->name('categorias.borrar');
+
+        route::put('categoria/{categoria}', 'update')->name('categorias.update');
+        
+        route::delete('categoria/{id}', 'borrar')->name('categorias.borrar');
+    
+        route::get('desactivarcategoria/{id}', 'desactivarcategoria')->name('desactivacategoria');
+    
+        route::get('activacategoria/{id}', 'activacategoria')->name('activacategoria');
+    });
+    Route::controller(perfilesController::class)->group(function(){
+
+        route::get('perfil', 'principal')->name('perfiles.principal');
+
+        route::get('perfil/crear', 'crear')->name('perfiles.crear');
+
+        route::post('perfil','store')->name('perfiles.store');
+    
+        route::get('perfil/{variable}/mostrar', 'mostrar')->name('perfiles.mostrar');
+
+        route::get('perfil/{perfil}/edit', 'editar')->name('perfiles.editar');
+
+        route::delete('perfil/{id}', 'borrar')->name('perfiles.borrar');
+
+        route::put('perfil/{perfil}', 'update')->name('perfiles.update');
+        
+        route::delete('perfil/{id}', 'borrar')->name('perfiles.borrar');
+    
+        route::get('desactivarperfil/{id}', 'desactivarperfil')->name('desactivaperfil');
+    
+        route::get('activaperfil/{id}', 'activaperfil')->name('activaperfil');
+    });
 });
